b1233205b80b10b31e4aef56031679bed87b9ada
shimuel <shimuel@yahoo.com>
2020-09-08 21:23:10 -0600

    port changes for VMs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import "./App.css";
 
-const apiUrl = 'http://localhost:3001';
+const apiUrl = 'http://10.100.199.91:3001';
+//const apiUrl = 'http://localhost:4001'
 function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
     <div
